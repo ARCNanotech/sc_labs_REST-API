ed8a5e28c16e1753e25afc42bff37ef65609d4ee
Arcana Magus <ArcanaMagus@users.noreply.github.com>
2015-08-21 02:06:04 +0100

    Updated config\boot.rb

--- connect/config/boot.rb
+++ connect/config/boot.rb
@@ -1,15 +1,25 @@
+# Do not modify this file
+# Configure your app in config/environment.rb and config/environments/*.rb
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 
 
+# simply use installed gems if available
+  def boot!
+    unless booted?
+      preinitialize
+      pick_boot.run
 
-def boot!
-  unless booted?
-    preinitialize
-    pick_boot.run
-end
+    puts 'using config/boot.rb'
+    ENV['RAILS_ENV'] = 'test'
+    require File.expand_path('app_root + '/config/boot')
+  else
+    puts 'using rubygems'
+    require 'rubygems'
+
+  
 
 
 
@@ -37,17 +47,19 @@ class boot
   def run
     load_initializer
     Rails::Initializer.run(:set_load_path)
-end
+
 
 
 class VendorBoot < boot
   def load_initializer
     require "#{[RAILS_ROOT]}/vendor/rails/railties/lib/initializer"
-  end
-end
+
+
 
 class GemBoot < boot
   def load_initializer
     self.class.load_rubygems
     load_rails_gem
     require 'initializer'
+  end
+end
