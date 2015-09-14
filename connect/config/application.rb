f346008ed2227e79dc52c5bcc1040a9a56fcaaaf
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-09-14 15:45:04 -0400

    Update file application.rb

--- connect/config/application.rb
+++ connect/config/application.rb
@@ -17,6 +17,9 @@ Bundler.require(*Rails.groups)
 
 module Connect
   class Application < Rails::Application
+  include TaskManager 
+  include TraceOutput 
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
